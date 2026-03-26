e9f1f7421bb90fdf9f03a6adcca7cba7c8f3ee8f
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-03-26 23:06:42 +0100

    add: Creation of the canvas and display of the rubber duck

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import React, { Suspense } from "react";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Loader } from "@react-three/drei";
+
 import Navbar from "./components/Navbar";
-import React from "react";
+import RubberDuck from "./components/RubberDuck";
 
 const App = () => {
+  return (
+    <div className="w-screen h-screen relative">
+      <Navbar />
 
-    return (
-        <Navbar/>
-    )
+      <Canvas>
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[2, 5, 2]} intensity={1} />
 
-}
+          <RubberDuck />
+
+        </Suspense>
+
+        <OrbitControls makeDefault />
+      </Canvas>
+
+      <Loader />
+    </div>
+  );
+};
 
 export default App;
