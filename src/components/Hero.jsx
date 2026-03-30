0630b4ea945b40d4deed1cfd556f1d27bf121746
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-03-30 22:12:27 +0200

    add: Get in Touch button to allow easy contact

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
+import { ArrowUpRight } from "lucide-react";
+
 const Name = () => {
   return (
-    <div className="text-[160px] font-header leading-30 mb-10">
+    <div className="text-[160px] leading-30 mb-10">
       <h1>YANN</h1>
       <h1>FERNANDEZ PUIG</h1>
     </div>
@@ -9,19 +11,39 @@ const Name = () => {
 
 const Desc = () => {
   return (
-    <div className="text-2xl font-header uppercase">
+    <div className="text-2xl uppercase">
       <h1>3rd year Computer Engineering student · EPITA</h1>
     </div>
   );
 };
 
+const Contact = () => {
+  return (
+    <a className="glass-morphism">
+      <div className="flex items-center px-7 py-3 gap-3 text-[#050505]">
+        <span className="uppercase font-header font-bold text-[20px]">Get in Touch</span>
+
+        <div className="bg-[#F8FBF8] rounded-full p-1 flex items-center justify-center">
+          <ArrowUpRight size={20} className="text-[#050505]" />
+        </div>
+
+      </div>
+    </a>
+  );
+};
+
 const Hero = () => {
   return (
-    <div className="w-full h-screen p-20 flex flex-col justify-between bg-amber-300">
+    <div className="w-full h-screen p-20 flex flex-col justify-between bg-mango-pop">
       <div></div>
+
       <div className="m-10">
         <Name />
-        <Desc />
+
+        <div className="flex justify-between">
+          <Desc />
+          <Contact />
+        </div>
       </div>
     </div>
   );
