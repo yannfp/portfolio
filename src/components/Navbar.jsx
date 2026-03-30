d205b4773e688809144f17cedc2e736613397e95
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-03-30 19:09:12 +0200

    feat: Navbar in liquid glass with cleaner UI and more minimal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const links = [
 
 const NavLinks = () => {
   return (
-    <div>
+    <div className="flex gap-10">
       {links.map(({ href, label }) => (
         <a key={href} href={href} className="px-5">
           {label}
@@ -20,9 +20,8 @@ const NavLinks = () => {
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50">
-      <div className="p-4 flex justify-between items-center">
-        <a href="/">Yann.Dev</a>
+    <nav className="fixed top-0 left-0 w-full z-50 flex justify-center m-5">
+      <div className="glass-morphism p-4 flex justify-center items-center w-2xl rounded-4xl">
         <NavLinks />
       </div>
     </nav>
