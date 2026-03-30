23d096a7a0f5a81390b6e801aad61998585e4b4d
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-03-30 20:09:06 +0200

    feat: Halo around Navbar to create some depth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,13 +18,25 @@ const NavLinks = () => {
   );
 };
 
+const Bar = () => {
+  return (
+    <div className="glass-morphism p-4 flex justify-center items-center w-2xl rounded-4xl">
+      <NavLinks />
+    </div>
+  );
+};
+
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 flex justify-center m-5">
-      <div className="glass-morphism p-4 flex justify-center items-center w-2xl rounded-4xl">
-        <NavLinks />
-      </div>
-    </nav>
+    <div className="relative">
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-200 h-30 bg-purple-600/40 blur-[80px] rounded-full -z-0 pointer-events-none" />
+
+      <nav className="fixed top-0 left-0 w-full flex justify-center z-50 pointer-events-none mt-6">
+        <div className="pointer-events-auto w-full max-w-2xl">
+          <Bar />
+        </div>
+      </nav>
+    </div>
   );
 };
 
