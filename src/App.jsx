00cb534de2da061a4b59fc2f06d3ae7fc5564ac2
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-04-01 22:23:10 +0200

    feat(App.jsx): Top line with a blur effect on it visible when scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ import About from "./components/About";
 const App = () => {
   return (
     <div className="w-screen h-screen">
+      <div
+        className="fixed top-0 left-0 w-full z-10 h-32 pointer-events-none transition-opacity duration-500"
+        style={{
+          backdropFilter: "blur(20px)",
+          WebkitBackdropFilter: "blur(20px)",
+          background:
+            "linear-gradient(to bottom, rgba(255,255,255,0.1), transparent)",
+          maskImage: "linear-gradient(to bottom, black 0%, transparent 100%)",
+          WebkitMaskImage:
+            "linear-gradient(to bottom, black 0%, transparent 100%)",
+        }}
+      />
       <Navbar />
       <Hero />
       <About />
