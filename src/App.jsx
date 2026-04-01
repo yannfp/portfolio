a315abee30ebb2476e484eb44526d78da2ff3420
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-04-01 21:46:10 +0200

    update: App.jsx
    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div className="w-screen h-screen">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
